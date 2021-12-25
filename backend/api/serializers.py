c5c491cb0e8d6c57a2c3ead545c828d0ba68fbbc
Kapkaev Andrey <kapkaew@yandex.ru>
2021-12-25 21:04:19 +0300

    Fix get_or_create

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -133,7 +133,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         ingredients = validated_data.pop('ingredients')
         tag_list = validated_data.pop('tags')
         super().update(instance, validated_data)
-        instance.ingredients.clear()
+        # instance.ingredients.clear()
         instance.tags.set(tag_list)
         instance = RecipeSerializer.get_and_update_instance(
             instance,
